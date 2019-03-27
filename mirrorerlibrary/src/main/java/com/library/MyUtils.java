cd822a36f388f08fd2f09d6da9c7752ecaa29f7e
zhangchaojun <565978459@qq.com>
2019-03-27 11:36:35 +0800

    首次提交

---
+++ mirrorerlibrary/src/main/java/com/library/MyUtils.java
@@ -0,0 +1,11 @@
+package com.library;
+
+import android.util.Log;
+
+public class MyUtils {
+    private static final String TAG = "cj";
+
+    public void log(){
+        Log.e(TAG, "log: 我是工具包" );
+    }
+}
